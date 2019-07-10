fd9c368862eed9493ecc924287c70a7242a6da9c
test <test@email.com>
2019-07-10 10:38:24 +0000

    CAM Demo

---
+++ JW-Project1/README.md
@@ -0,0 +1,8 @@
+## project1 infrastructure
+
+Created 7/10/19 by admin. For Cloud vCenter for project1
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ JW-Project1/variables.tf
@@ -0,0 +1,71 @@
+#####################################################################
+##
+##      Created 7/10/19 by admin. For Cloud vCenter for project1
+##
+#####################################################################
+
+variable "user" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "vsphere_server" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "allow_unverified_ssl" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "camdemo_name" {
+  type = "string"
+  description = "Virtual machine name for camdemo"
+}
+
+variable "camdemo_number_of_vcpu" {
+  type = "string"
+  description = "Number of virtual cpu's."
+}
+
+variable "camdemo_memory" {
+  type = "string"
+  description = "Memory allocation."
+}
+
+variable "camdemo_disk_name" {
+  type = "string"
+  description = "The name of the disk. Forces a new disk if changed. This should only be a longer path if attaching an external disk."
+}
+
+variable "camdemo_disk_size" {
+  type = "string"
+  description = "The size of the disk, in GiB."
+}
+
+variable "camdemo_template_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "camdemo_datacenter_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "camdemo_datastore_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "camdemo_resource_pool" {
+  type = "string"
+  description = "Resource pool."
+}
+
